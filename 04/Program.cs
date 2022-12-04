161a50cbd3e54b4aa1e86e8c84154738ae47ff90
Arve Svendsen <arve.svendsen@gmail.com>
2022-12-04 22:31:30 +0100

    Luke 4

---
+++ 04/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int sum = 0;
+
+for (int i = 1; i < 10000000; i++)
+{
+    if (Util.Tool.IsMultiPalindriom(i))
+    {
+        sum += i;
+    }
+
+
+}
+
+Console.WriteLine(sum);
