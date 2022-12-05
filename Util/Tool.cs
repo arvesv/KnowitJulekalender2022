8b7aae312c697ba1a6ec3435f0736aa409fdac22
Arve Svendsen <arve.svendsen@gmail.com>
2022-12-05 23:06:23 +0100

    04

---
+++ Util/Tool.cs
@@ -0,0 +1,68 @@
+﻿namespace Util
+{
+    public class Tool
+    {
+        public static string PrintNumInBase(int num, int bas)
+        {
+            string number = "";
+
+            do
+            {
+                int m = num % bas;
+
+                if(m < 10)
+                    number = m.ToString() + number;
+                else
+                {
+                    char c = "ABCDEF"[m - 10];
+                    number = c + number;
+                }
+                num = num / bas;
+            } while (num != 0);
+
+            return number;
+        }
+
+        public static bool IsPalindrom(string num)
+        {
+            int start = 0;
+            int end = num.Length -1;
+
+
+            while (start <= end)
+            {
+                if (num[start] != num[end])
+                    return false;
+
+                start++;
+                end--;
+
+            }
+
+            return true;
+        }
+
+        public static bool IsMultiPalindriom(int num)
+        {
+            int noBases = 0;
+
+            for (int i = 2; i <= 16; i++)
+            {
+                var s = PrintNumInBase(num, i);
+                if (IsPalindrom(s))
+                {
+                    noBases++;
+                }
+
+                if (noBases >= 3)
+                {
+                    return true;
+                }
+
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UtilTests/ToolTests.cs
@@ -0,0 +1,26 @@
+﻿using Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Util.Tests
+{
+    [TestClass()]
+    public class ToolTests
+    {
+        [TestMethod()]
+        public void IsPalindromTest()
+        {
+            Assert.IsTrue(Util.Tool.IsPalindrom("12321"));
+            Assert.IsFalse(Util.Tool.IsPalindrom("12"));
+
+        }
+
+        [TestMethod()]
+        public void IsMultiPalindriomTest()
+        {
+            Assert.IsTrue(Util.Tool.IsMultiPalindriom(9));
+            Assert.IsFalse(Util.Tool.IsMultiPalindriom(8));
+
+
+        }
+    }
+}
